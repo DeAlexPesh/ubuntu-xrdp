f9c6e34891d5cec16bfd76be6b6d40b7f1540aaa
DeAlexPesh <DeAlexWin@gmail.com>
2021-05-24 17:43:29 +0500

    Create chrome-run.sh

---
+++ bin/chrome-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[ -f ~/.config/chromium/'Local 2State' ] && \
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Local State'
+
+[ -f ~/.config/chromium/Default/Preferences ] && \
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
+
+chromium-browser --no-sandbox --disable-gpu --no-first-run --start-maximized \
+--incognito --test-type \
+"https://olimpoks.ente-ltd.ru/"
