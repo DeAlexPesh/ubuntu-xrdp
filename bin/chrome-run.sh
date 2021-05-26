4dba8504815f3e0b0141d26d4f3c2646e29f437b
DeAlexPesh <DeAlexWin@gmail.com>
2021-05-26 18:01:13 +0500

    Update chrome-run.sh

--- bin/chrome-run.sh
+++ bin/chrome-run.sh
@@ -7,5 +7,5 @@ sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' ~/.config/chromium/'Loc
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/' ~/.config/chromium/Default/Preferences
 
 chromium-browser --no-sandbox --disable-gpu --no-first-run \
---kiosk --test-type \
+--test-type --load-extension="/extensions/navi" \
 "$KIOSKURL"
