eaeb46d21c7da4b8ec311e23b10d2485b0a6bdec
DeAlexPesh <DeAlexWin@gmail.com>
2021-05-28 00:52:27 +0500

    Update auto-logout.sh

--- bin/auto-logout.sh
+++ bin/auto-logout.sh
@@ -1,3 +1,20 @@
-#!/bin/sh
-if [ "$IDLETIME" ]; then xautolock -time "$IDLETIME" -locker pkill -u "$USER"; fi
-# ncat -l -p 5555 --allow 127.0.0.1 -e pkill -u "$USER"
+#!/bin/bash
+
+displays=$(ps aux | grep Xvnc | grep -v 'grep\|sed' | sed -r 's|.*(Xvnc :[0-9]*).*|\1|' | cut -d' ' -f 2)
+
+while read -r d; do
+  export DISPLAY=$d
+  idle=$(xprintidle)
+
+  if ! [ "$idle" -eq "$idle" ] 2>/dev/null; then exit; fi
+
+  idleMins=$((idle/1000/60))
+  if [[ $idleMins -gt "$IDLETIME" ]]; then
+    PID=$(pgrep -f "vnc $d")
+    kill -HUP "$PID"
+    
+    FNAME=$(echo $d | sed -e 's/:/X/g')
+    FILENAME="/tmp/.X11-unix/$FNAME"
+    rm -f "$FILENAME"
+  fi  
+done <<< "$displays"
