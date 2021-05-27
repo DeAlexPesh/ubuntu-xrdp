b7bb044c9166e24b0962bd5fbd0753d2755bfa8f
DeAlexPesh <DeAlexWin@gmail.com>
2021-05-27 22:52:28 +0500

    Update auto-logout.sh

--- bin/auto-logout.sh
+++ bin/auto-logout.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 if [ "$IDLETIME" ]; then xautolock -time "$IDLETIME" -locker pkill -u "$USER"; fi
-while true; do { printf 'HTTP/1.1 200 OK\r\n'; sh pkill -u "$USER"; } | nc -l 8080; done
+# ncat -l -p 5555 --allow 127.0.0.1 -e pkill -u "$USER"
