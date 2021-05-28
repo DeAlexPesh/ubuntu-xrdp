22114aeb886145fe98c5b27db64ab963cdb79d6b
DeAlexPesh <DeAlexWin@gmail.com>
2021-05-28 14:27:39 +0500

    Update background.js

--- extensions/navi/background.js
+++ extensions/navi/background.js
@@ -1,11 +1,7 @@
-chrome.runtime.onMessage.addListener(
- function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.greeting == "close") {
    chrome.tabs.query({}, function (tabs) {
-    for (var i = 0; i < tabs.length; i++) {
-     chrome.tabs.remove(tabs[i].id);
-    }
+    for (var i = 0; i < tabs.length; i++) { chrome.tabs.remove(tabs[i].id); }
    });
   }
- }
-);
+});
