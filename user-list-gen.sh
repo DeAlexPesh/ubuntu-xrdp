476c7899f3b9e0140951b9c901692683baef4375
DeAlexPesh <DeAlexWin@gmail.com>
2021-05-28 22:17:25 +0500

    Create user-list-gen.sh

---
+++ user-list-gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+ printf '\nUsage: user-list-gen.sh X Y  ( X is need users quantity, Y is users.list file... )\n\n'
+ exit 0
+fi
+
+cat '' > "$2"
+
+for (( I=1; I<="$1"; I++ )); do  
+ NMUSER=$(printf "link%02d" I)
+ cat "$((1000+I)) $NMUSER $(openssl passwd -1 "$NMUSER") sudo" >> "$2"
+done
